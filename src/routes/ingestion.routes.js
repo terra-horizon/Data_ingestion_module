09aed0cfe0c33911a7f95afb55cfb89b13a4cc46
Dimitris Papathanasiou <dimitrispapathanasioy00@gmail.com>
2026-08-14 08:01:29 +0300

    Remove the legacy Node.js ingestion service

--- src/routes/ingestion.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const asyncHandler = require('express-async-handler');
-const ingestionController = require('../controllers/ingestion.controller');
-
-const router = express.Router();
-
-router.post('/run', asyncHandler(ingestionController.runIngestion));
-
-module.exports = router;
